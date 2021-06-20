309cb30292097478cc198067cfedb44225773fcd
Lukas Bach <lbach@outlook.de>
2021-06-20 19:11:54 +0200

    docs package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,5 @@
 export * from './controlledEnvironment/ControlledTreeEnvironment';
+export * from './tree/Tree';
+export * from './uncontrolledEnvironment/UncontrolledTreeEnvironment';
+export * from './uncontrolledEnvironment/StaticTreeDataProvider';
 export * from './types';
---
+++ packages/docs/src/components/PropTable.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { useDynamicImport } from 'docusaurus-plugin-react-docgen-typescript/pkg/dist-src/hooks/useDynamicImport';
+
+export const PropTable = ({ name }) => {
+  const props = useDynamicImport(name);
+  console.log(props, name)
+
+  if (!props) {
+    return null;
+  }
+
+  return (
+    <table>
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Type</th>
+        <th>Default Value</th>
+        <th>Required</th>
+        <th>Description</th>
+      </tr>
+      </thead>
+      <tbody>
+      {Object.keys(props).map(key => {
+        return (
+          <tr key={key}>
+            <td>
+              <code>{key}</code>
+            </td>
+            <td>
+              <code>{props[key].type?.name}</code>
+            </td>
+            <td>
+              {props[key].defaultValue && (
+                <code>{props[key].defaultValue.value}</code>
+              )}
+            </td>
+            <td>{props[key].required ? 'Yes' : 'No'}</td>
+            <td>{props[key].description}</td>
+          </tr>
+        );
+      })}
+      </tbody>
+    </table>
+  );
+};
