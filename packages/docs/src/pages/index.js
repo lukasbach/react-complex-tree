c102259cef77047a4ce1415236a07409e103df85
Lukas Bach <lbach@outlook.de>
2021-07-03 18:05:22 +0200

    docs: homepage features

--- packages/docs/src/pages/index.js
+++ packages/docs/src/pages/index.js
@@ -12,26 +12,32 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--primary button--lg"
-            to="/docs/getstarted">
-            Get Started
-          </Link>
-          <a
-            className="button button--secondary button--lg"
-            href="/storybook">
-            More Demos
-          </a>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/api">
-            API
-          </Link>
+        <div className="row">
+          <div className={clsx('col col--5', styles.heroBannerText)}>
+            <h1 className="hero__title">{siteConfig.title}</h1>
+            <p className="hero__subtitle">{siteConfig.tagline}</p>
+            <div className={styles.buttons}>
+              <Link
+                className="button button--primary button--lg"
+                to="/docs/getstarted">
+                Get Started
+              </Link>
+              <a
+                className="button button--secondary button--lg"
+                href="/storybook">
+                More Demos
+              </a>
+              <Link
+                className="button button--secondary button--lg"
+                to="/docs/api">
+                API
+              </Link>
+            </div>
+          </div>
+          <div className="col col--7">
+            <StoryEmbed storyName="auto-demo-autodemo-component--multi-tree-demo" iframeProps={{ minHeight: 380, width: 600 }} />
+          </div>
         </div>
-        <StoryEmbed storyName="auto-demo-autodemo-component--multi-tree-demo" iframeProps={{ minHeight: 380, width: 600 }} />
       </div>
     </header>
   );
