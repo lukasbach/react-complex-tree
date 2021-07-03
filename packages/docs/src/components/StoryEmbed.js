fe0abab5b69340b15b94e0811b65e44dc7869659
Lukas Bach <lbach@outlook.de>
2021-07-03 18:23:29 +0200

    docs: fix story embed on prod

--- packages/docs/src/components/StoryEmbed.js
+++ packages/docs/src/components/StoryEmbed.js
@@ -2,10 +2,11 @@ import * as React from 'react';
 import IframeResizer from 'iframe-resizer-react';
 
 export const StoryEmbed = ({ storyName, iframeProps }) => {
+  const baseUrl = process.env.NODE_ENV === 'development' ? `http://localhost:6006` : `/storybook`;
 
   return (
     <IframeResizer
-      src={`http://localhost:6006/iframe.html?id=${storyName}&args=&viewMode=story`}
+      src={`${baseUrl}/iframe.html?id=${storyName}&args=&viewMode=story`}
       frameBorder={0}
       allowtransparency="true"
       style={(!iframeProps.width && !iframeProps.minWidth && !iframeProps.maxWidth) ? { width: '1px', minWidth: '100%' } : {}}
