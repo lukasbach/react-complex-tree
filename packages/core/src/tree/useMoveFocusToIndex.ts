8e81abf56299c24416b1251f2f0ce9bf09b9f78f
Lukas Bach <lbach@outlook.de>
2022-02-01 02:00:24 +0100

    feat: improve useMoveFocusToIndex

--- packages/core/src/tree/useMoveFocusToIndex.ts
+++ packages/core/src/tree/useMoveFocusToIndex.ts
@@ -3,20 +3,21 @@ import { useGetLinearItems } from './useGetLinearItems';
 import { useViewState } from './useViewState';
 import { useTree } from './Tree';
 import { useTreeEnvironment } from '../controlledEnvironment/ControlledTreeEnvironment';
+import { useCallback } from 'react';
 
 export const useMoveFocusToIndex = () => {
   const { treeId } = useTree();
-  const environment = useTreeEnvironment();
+  const { onFocusItem, items } = useTreeEnvironment();
   const getLinearItems = useGetLinearItems();
   const viewState = useViewState();
 
-  return (computeNewIndex: (currentIndex: number, linearItems: ReturnType<typeof getItemsLinearly>) => number) => {
+  return useCallback((computeNewIndex: (currentIndex: number, linearItems: ReturnType<typeof getItemsLinearly>) => number) => {
     const linearItems = getLinearItems();
     const currentIndex = linearItems.findIndex(item => item.item === viewState.focusedItem) ?? 0;
     const newIndex = computeNewIndex(currentIndex, linearItems);
     const newIndexBounded = Math.max(0, Math.min(linearItems.length - 1, newIndex));
-    const newFocusItem = environment.items[linearItems[newIndexBounded].item];
-    environment.onFocusItem?.(newFocusItem, treeId);
+    const newFocusItem = items[linearItems[newIndexBounded].item];
+    onFocusItem?.(newFocusItem, treeId);
     return newFocusItem;
-  };
+  }, [onFocusItem, items, getLinearItems, treeId, viewState.focusedItem]);
 };
