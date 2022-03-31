baa637731ca5794ea0f4c2f86d81141ccb6aaddb
Lukas Bach <lbach@outlook.de>
2022-03-31 22:09:32 +0200

    feat: improve memoization in drag handler hook

--- packages/core/src/controlledEnvironment/useOnDragOverTreeHandler.ts
+++ packages/core/src/controlledEnvironment/useOnDragOverTreeHandler.ts
@@ -8,13 +8,13 @@ const isOutsideOfContainer = (e: DragEvent, treeBb: DOMRect) => {
   return e.clientX < treeBb.left || e.clientX > treeBb.right || e.clientY < treeBb.top || e.clientY > treeBb.bottom;
 };
 
-const getHoveringPosition = (clientY: number, treeTop: number, itemHeight: number, capabilities: TreeCapabilities) => {
+const getHoveringPosition = (clientY: number, treeTop: number, itemHeight: number, canDropOnItemWithChildren?: boolean, canDropOnItemWithoutChildren?: boolean) => {
   const hoveringPosition = (clientY - treeTop) / itemHeight;
 
   const linearIndex = Math.floor(hoveringPosition);
   let offset: 'top' | 'bottom' | undefined = undefined;
 
-  const lineThreshold = capabilities.canDropOnItemWithChildren || capabilities.canDropOnItemWithoutChildren ? 0.2 : 0.5;
+  const lineThreshold = canDropOnItemWithChildren || canDropOnItemWithoutChildren ? 0.2 : 0.5;
 
   if (hoveringPosition % 1 < lineThreshold) {
     offset = 'top';
@@ -32,12 +32,20 @@ export const useOnDragOverTreeHandler = (
   onDragAtPosition: (draggingPosition: DraggingPosition | undefined) => void,
   onPerformDrag: (draggingPosition: DraggingPosition) => void
 ) => {
-  const environment = useTreeEnvironment();
+  const {
+    canDropOnItemWithChildren,
+    canDropOnItemWithoutChildren,
+    canDragAndDrop,
+    linearItems,
+    items,
+    canReorderItems,
+    viewState,
+  } = useTreeEnvironment();
   const getParentOfLinearItem = useGetGetParentOfLinearItem();
 
   return useCallback(
     (e: DragEvent, treeId: string, containerRef: React.MutableRefObject<HTMLElement | undefined>) => {
-      if (!environment.canDragAndDrop) {
+      if (!canDragAndDrop) {
         return;
       }
 
@@ -52,7 +60,7 @@ export const useOnDragOverTreeHandler = (
       const treeBb = containerRef.current.getBoundingClientRect();
       const outsideContainer = isOutsideOfContainer(e, treeBb);
 
-      let { linearIndex, offset } = getHoveringPosition(e.clientY, treeBb.top, itemHeight, environment);
+      let { linearIndex, offset } = getHoveringPosition(e.clientY, treeBb.top, itemHeight, canDropOnItemWithChildren, canDropOnItemWithoutChildren);
 
       const nextDragCode = outsideContainer ? 'outside' : `${treeId}${linearIndex}${offset ?? ''}`;
 
@@ -67,40 +75,40 @@ export const useOnDragOverTreeHandler = (
         return;
       }
 
-      if (linearIndex < 0 || linearIndex >= environment.linearItems[treeId].length) {
+      if (linearIndex < 0 || linearIndex >= linearItems[treeId].length) {
         onDragAtPosition(undefined);
         return;
       }
 
-      const targetItem = environment.linearItems[treeId][linearIndex];
+      const targetItem = linearItems[treeId][linearIndex];
       const depth = targetItem.depth;
-      const targetItemData = environment.items[targetItem.item];
+      const targetItemData = items[targetItem.item];
 
-      if (!offset && !environment.canDropOnItemWithoutChildren && !targetItemData.hasChildren) {
+      if (!offset && !canDropOnItemWithoutChildren && !targetItemData.hasChildren) {
         onDragAtPosition(undefined);
         return;
       }
 
-      if (!offset && !environment.canDropOnItemWithChildren && targetItemData.hasChildren) {
+      if (!offset && !canDropOnItemWithChildren && targetItemData.hasChildren) {
         onDragAtPosition(undefined);
         return;
       }
 
-      if (offset && !environment.canReorderItems) {
+      if (offset && !canReorderItems) {
         onDragAtPosition(undefined);
         return;
       }
 
       const parent = getParentOfLinearItem(linearIndex, treeId);
 
-      if (environment.viewState[treeId]?.selectedItems?.includes(targetItem.item)) {
+      if (viewState[treeId]?.selectedItems?.includes(targetItem.item)) {
         return;
       }
 
       const newChildIndex =
-        environment.items[parent.item].children!.indexOf(targetItem.item) + (offset === 'top' ? 0 : 1);
+        items[parent.item].children!.indexOf(targetItem.item) + (offset === 'top' ? 0 : 1);
 
-      if (offset === 'top' && depth === (environment.linearItems[treeId][linearIndex - 1]?.depth ?? -1)) {
+      if (offset === 'top' && depth === (linearItems[treeId][linearIndex - 1]?.depth ?? -1)) {
         offset = 'bottom';
         linearIndex -= 1;
       }
@@ -132,6 +140,6 @@ export const useOnDragOverTreeHandler = (
 
       onPerformDrag(draggingPosition);
     },
-    [environment, getParentOfLinearItem, itemHeight, lastDragCode, onDragAtPosition, onPerformDrag, setLastDragCode]
+    [canDragAndDrop, canDropOnItemWithChildren, canDropOnItemWithoutChildren, canReorderItems, getParentOfLinearItem, itemHeight, items, lastDragCode, linearItems, onDragAtPosition, onPerformDrag, setLastDragCode, viewState]
   );
 };
